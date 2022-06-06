0cac36e5d14cbd728eff3d6365ae215456ff41fb
DavidKeszei <76428399+DavidKeszei@users.noreply.github.com>
2022-06-06 17:14:35 +0200

    First commit

---
+++ example/menetrendek_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:menetrendek_api/menetrendek_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/menetrendek_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library menetrendek_api;
+
+export 'src/menetrendek_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
